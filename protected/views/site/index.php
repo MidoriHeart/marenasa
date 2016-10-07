427a3cf60143f2ffbdb5da3f92358ed814afc10a
MidoriHeart <midori.wings@gmail.com>
2016-10-07 16:07:47 -0700

    inicio.

--- protected/views/site/index.php
+++ protected/views/site/index.php
@@ -7,21 +7,91 @@
 $this->pageTitle=Yii::app()->name;
 ?>
 <div class="container1">
+	<div class="flechaIzq"></div>
+		<div class="container11">
+			<label class="titulo1">TITULO SIMULADO</label>
+			<label class="subitutlo1">Donec rutrum congue leo eget malesuada. Vivamus suscipit tortor eget felis porttitor volutpat. </label>
+			<div class= "botonVer"> Ver más</div>	
+		</div>
+			<div class ="image1-carousel"></div>
+		<div class="flechaDer"></div>
 </div>
 <div class="container2">
+	<div class="pContainer">
+		     <div class ="oferta">
+		     		<div class="letreror">-50%</div>
+		     		<div class="ofertal">
+		     			<label class ="subtitulo21">OFERTA ESPECIAL</label>
+		     				<label class="subtitulo22">lorem Ipsum</label>
+		     		</div>
+		     		<div class="imagenOferta"></div>
+		     		<div class="botonVerde">Ver más...</div>
+		     </div>		 
+		     	<label class="letrerosl2">Productos Estreslla</label>
+		 <div class="carousel-wrapper">
+
+		 	<div class="flechaISl2"></div>
+			<div class ="sproducto"><div class="images1"></div></div>
+			<div class ="sproducto"><div class="images2"></div></div>
+			<div class ="sproducto"><div class="images3"></div></div>
+			<div class="flechaDSl2"></div>
+		</div>
+	</div> 	
+ </div>
 </div>
 <div class="container3">
-	<div class="categoriasl"></div>
-	<div class="categoriasl"> </div>
-	<div class="categoriasr"></div>
-	<div class="categoriasr"></div>
+	<div class="pContainer">
+		<div class="categoriasl">
+			<div class="imagenCat"></div>
+			<div class="botonVerdeC">Ver más</div>
+			<label class="letreroCat">Categoria 1</label>
+		</div>
+		<div class="categoriasl">		
+			<div class="imagenCat1"></div>
+			<div class="botonVerdeC">Ver más</div>
+			<label class="letreroCat">Categoria 2</label> 
+		</div>
+		<div class="categoriasl">
+			<div class="imagenCat2"></div>
+			<div class="botonVerdeC">Ver más</div>
+			<label class="letreroCat">Categoria 3</label>
+		</div>
+		<div class="categoriasl">
+			<div class="imagenCat3"></div>
+			<div class="botonVerdeC">Ver más</div>
+			<label class="letreroCat">Categoria 4</label>
+		</div>
+		<label class="letrarofCat">MÁS CATEGORÍAS</labeL>
+		<div class="flechaCat"></div>
+	</div>
 </div>
 <div class="container4">
-	<div class ="titulares4"></div>
-	<div class ="titulares4"></div>
-	<div class ="titulares4"></div>
-	<div class ="titulares4"></div>
-
+	<div class="pContainer">
+		<div class ="titulares4">
+			<label class="titulo41">Nosotros</label>
+			<label class="titulo42">Subtitulo</label>
+			<div class="linea41"></div>
+			<label class="titulo43 ">Vivamus magna justo, lacinia eget consectetur sed, convallis at tellus. Donec sollicitudin molestie malesuada. Vestibulum ac diam sit </label>
+			<label class="titulo44">Leer más</label>
+		</div>
+		<div class ="titulares4">
+			<label class="titulo41">Nosotros</label>
+			<label class="titulo42">Subtitulo</label>
+			<div class="linea41"></div>
+			<label class="titulo43 ">Vivamus magna justo, lacinia eget consectetur sed, convallis at tellus. Donec sollicitudin molestie malesuada. Vestibulum ac diam sit  </label>
+			<label class="titulo44">Leer más</label></div>
+		<div class ="titulares4">
+			<label class="titulo41">Nosotros</label>
+			<label class="titulo42">Subtitulo</label>
+			<div class="linea41"></div>
+			<label class="titulo43 ">Vivamus magna justo, lacinia eget consectetur sed, convallis at tellus. Donec sollicitudin molestie malesuada. Vestibulum ac diam sit</label>
+			<label class="titulo44">Leer más</label></div>
+		<div class ="titulares4">
+    		<label class="titulo41">Nosotros</label>
+			<label class="titulo42">Subtitulo</label>
+			<div class="image411"></div>
+			<label class="titulo44">Leer más</label></div>
+	</div>
 </div>
 
 
