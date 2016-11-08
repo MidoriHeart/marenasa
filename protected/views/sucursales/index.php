3ef57c493c567d61dc5787633413d2fe9f968966
MidoriHeart <midori.wings@gmail.com>
2016-11-08 15:04:34 -0800

    arriba.

---
+++ protected/views/sucursales/index.php
@@ -0,0 +1,13 @@
+<?php
+$baseUrl = Yii::app()->baseUrl;
+    $cs = Yii::app()->getClientScript();
+    $cs->registerCoreScript('jquery');
+    $cs->registerCssFile($baseUrl.'/css/sucursales/sucursales.css');
+?>
+<div class= "banner-scl">
+	<div class="titulo1-scl">Sucursales</div>
+</div>
+
+<div class="contenedor-scl">
+
+</div>
