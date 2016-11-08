6736f0bb0fe1c89040327b0e8ab03ec96cadd1cc
MidoriHeart <midori.wings@gmail.com>
2016-11-08 13:59:47 -0800

    arriba.

--- protected/views/layouts/main.php
+++ protected/views/layouts/main.php
@@ -38,13 +38,17 @@
 			'items'=>array(
 				array('label'=>'Inicio', 'url'=>array('/site/index')),
 				array('label'=>'Quienes Somos', 'url'=>array('/quienesomos')),
-				array('label'=>'Preguntas Frecuentes', 'url'=>array('/preguntasfrecuentes'))
+				array('label'=>'Preguntas Frecuentes', 'url'=>array('/preguntasfrecuentes')),
+				array('label'=>'Contacto', 'url'=>array('/contacto')),
+				array('label'=>'Mision y Vision', 'url'=>array('/misionvision')),
+
+				),
 			/*	array('label'=>'Nosotros', 'url'=>array('/site/page', 'view'=>'about')),
 				array('label'=>'Productos', 'url'=>array('/site/contact')),
 				array('label'=>'Promociones', 'url'=>array('/site/contact')),
 				array('label'=>'Servicios', 'url'=>array('/site/login'), 'visible'=>Yii::app()->user->isGuest),
      			array('label'=>'Sucursales', 'url'=>array('/site/login'), 'visible'=>Yii::app()->user->isGuest),
-				array('label'=>'Contactos', 'url'=>array('/site/login'), 'visible'=>Yii::app()->user->isGuest),	*/		),
+				array('label'=>'Contactos', 'url'=>array('/site/login'), 'visible'=>Yii::app()->user->isGuest),	*/		
 		)); ?>
 	</div>
 	</div><!-- mainmenu -->
