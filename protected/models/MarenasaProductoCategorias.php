791c615762768b22d7c7b84742bd45f5efd8a6c2
Omar Peralta <ob.peralta03@gmail.com>
2016-11-12 12:09:41 -0800

    categorias, productos y marcas

--- protected/models/MarenasaProductoCategorias.php
+++ protected/models/MarenasaProductoCategorias.php
@@ -117,6 +117,36 @@ class MarenasaProductoCategorias extends MActiveRecord
         $b = Yii::app()->baseUrl;
         return "<div class='imagenWidget' style='background-image:url($b/uploads/marenasaproductocategorias/imagen/$nombre)'></div>";
     }
+
+	public function extraOptions() 
+    {
+        $array = array
+        (
+            'imagen'=>array
+            (
+                'class'=>'span5 jcropImage'
+            ),            
+        );
+        return $array;
+    }
+    public function js() 
+    {
+        $baseUrl = Yii::app()->baseUrl;
+        $cs = Yii::app()->getClientScript();        
+        $cs->registerScriptFile($baseUrl.'/js/plugins/JCrop/jquery.Jcrop.min.js');
+        $cs->registerScriptFile($baseUrl.'/js/plugins/ColorBox/jquery.colorbox.js');
+        $cs->registerScriptFile($baseUrl.'/js/ycm/productocategorias/cutImage.js');
+    }
+    public function css() 
+    {
+        $baseUrl = Yii::app()->baseUrl;
+        $cs = Yii::app()->getClientScript();
+        $cs->registerCssFile($baseUrl.'/css/plugins/JCrop/jquery.Jcrop.min.css');
+        $cs->registerCssFile($baseUrl.'/js/plugins/ColorBox/colorbox.css');
+        $cs->registerCssFile($baseUrl.'/css/ycm/productoscategoria/cutImage.css');
+        $cs->registerCssFile($baseUrl.'/css/ycm/productoscategoria/list.css');
+    }
+
     public function adminSearch()
     {
         return array
@@ -137,5 +167,151 @@ class MarenasaProductoCategorias extends MActiveRecord
                 ),
             )
         );
+    }
+    public function extraHtml() 
+    {
+        $array = array
+        (
+            'imagen'=>'
+                    <!-- # FancyBox y jcrop div # -->
+                    <a id="clrbx" href="#jcrop"></a>
+                    <div style="display:none;">
+                        <div id="jcrop">
+                            <table>
+                                <tr>
+                                    <td id="image_container">
+                                    <img src="#" id="selected_image" alt="Imagen seleccionada"/>
+                                    </td>
+                                                                </tr>
+                                                                <tr>
+                                    <td>
+                                    <div id="miniPreview" style="">
+                                        <img src="#" id="preview" alt="vista previa" class="jcrop-preview" style="background-color:transparent;" />
+                                    </div>
+                                    <div id="miniButton">
+                                        <input type="button" id="getCrop" value="Recortar Imagen" />
+                                        <input type="hidden" id="x" name="x" value="" /> 
+                                        <input type="hidden" id="y" name="y" value="" /> 
+                                        <input type="hidden" id="w" name="w" value="" /> 
+                                        <input type="hidden" id="h" name="h" value="" /> 
+                                        <input type="hidden" id="divwidth" name="divwidth" value="" /> 
+                                        <input type="hidden" id="divheight" name="divheight" value="" /> 
+                                    </div>
+                                    </td>
+                                </tr>
+                                <tr>
+                                    <td style="text-align:right;" colspan="2">
+                                    </td>
+                                </tr>
+                            </table>
+                        </div>  
+                    </div>
+                    '
+            );
+        return $array;
+    }
+    public function extraPhpBeforeSaveValidate($model='',$post='',$paths='',$module='')
+    {
+    }
+    public function extraPhpAfterSaveValidate($model='',$post='',$paths='', $module='')
+    {   
+        if($paths != null && $paths != '')
+        {
+            for($i=0; $i < count($paths); $i++)
+            {
+                if(!isset($post['x'],$post['y'],$post['w'],$post['h']))
+                    throw new CHttpException(400,"No fue posible recortar la imagen, los parametros de recorte son invalidos");
+                //revisa que la imagen funte exista en el servidor
+                if(!file_exists($paths[$i]))
+                    throw new CHttpException(500,"La imagen no ha sido cargada correctamente");
+                //si no trae el ancho y el largo intenta mover $imagen a la ruta de $nuevaImagen
+                //tal cual sin recortar devido a que los parametros de recorte no son suficientes
+                if(empty($post['w']) || empty($post['h'])){
+                    return move_uploaded_file($paths[$i],$paths[$i]);
+                }
+                //width y height que tendra la imagen generada
+                $width=$post["divwidth"];
+                $height=$post["divheight"];
+                //crea una copia de la imagen en memoria
+                $_SESSION["tipo"] = '';
+                $_SESSION["creado"] = '';
+                $ext = pathinfo($paths[$i], PATHINFO_EXTENSION);
+                $type = strtolower($ext);
+                if ($type === 'jpeg') {$type = 'jpg';}
+                switch($type){
+                        case 'bmp': $img_r = imagecreatefromwbmp($paths[$i]); break;
+                        case 'gif': $img_r = imagecreatefromgif($paths[$i]); break;
+                        case 'jpg': $img_r = imagecreatefromjpeg($paths[$i]); break;
+                        case 'png': $img_r = imagecreatefrompng($paths[$i]); break;
+                        default : return "Unsupported picture type!, Please use one of the following: bmp, gif, jpg, jpeg or png";
+                }
+                $_SESSION["tipo"] = $type;
+                //crea una nueva imagen en memoria del tamaño definido por width y height
+                $dst_r = imagecreatetruecolor( (int)$width, (int)$height );
+                //calidad de la imagen final
+                $calidad=100;
+                // intentamos preservar la transparencia
+                if($type == "gif" || $type == "png"){
+                        $r = imagecolortransparent($dst_r, imagecolorallocatealpha($img_r, 0, 0, 0, 127));
+                        $_SESSION["creado"] = $r;
+                        imagealphablending($dst_r, false);
+                        imagesavealpha($dst_r, true);
+                }
+                //crea una imagen en memoria en base a la imagen fuente con los datos de recorte
+                $retorno= imagecopyresampled($dst_r,$img_r,0,0,(int)$post['x'],(int)$post['y'],
+                    (int)$width,(int)$height,(int)$post['w'],(int)$post['h']);
+                //si no logro crear la co[ia de la imagen en memoria
+                if($retorno === false){
+                    return false;
+                }
+                $n = strpos($paths[$i], "imagen");  //variable de imagen
+                $new_path = substr($paths[$i], 0, $n);
+                //si la imagen es cambiada con un update, toma la imagen anterior y la intenta borrar
+
+                /* Nuevo PATH */
+//                $new_path = $module->getAttributePath(__CLASS__,'logo').DIRECTORY_SEPARATOR.'cutted_image'.DIRECTORY_SEPARATOR;
+                $new_path = $module->getAttributePath(__CLASS__,'imagen').DIRECTORY_SEPARATOR;
+
+                if($this->nombre_anterior2 != null)
+                {
+                    $baseUrl = Yii::app()->baseUrl;
+                    $targetFile = $baseUrl."uploads/marenasaproductocategorias/imagen/{$this->nombre_anterior2}";
+                    unlink(realpath($targetFile));
+                }
+                $new_path = $new_path.$model->imagen;
+                //intenta guardar la imagen resultante en el servidor
+                                switch($type){
+                                        case 'bmp': $retorno = imagewbmp($dst_r, $new_path); break;
+                                        case 'gif': $retorno = imagegif($dst_r, $new_path) ; break;
+                                        case 'jpg': $retorno = imagejpeg($dst_r, $new_path,$calidad); break;
+                                        case 'png': $retorno = imagepng($dst_r, $new_path) ; break;
+                                }
+                if($retorno === false){
+                    return false;
+                }
+            }
+        }
+        else
+        {
+             $columnas = array
+             (
+                 'imagen' => $model->nombre_anterior
+             );
+             $update = Yii::app()->db->createCommand()
+                 ->update('marenasa_producto_categorias', $columnas, "id = ".(int)$model->id);
+        }
+    }
+    public function afterFind()
+    {
+        if($this->imagen != '' && $this->imagen != null)
+        {
+            $this->nombre_anterior = $this->imagen;
+            $this->nombre_anterior2 = $this->imagen;
+        }
+        else
+        {
+            $this->nombre_anterior = null;
+            $this->$nombre_anterior2 = null;
+        }
     }
 }
--- protected/models/MarenasaProductoMarcas.php
+++ protected/models/MarenasaProductoMarcas.php
@@ -13,6 +13,15 @@
  */
 class MarenasaProductoMarcas extends MActiveRecord
 {
+    public $adminNames=array('Marcas de productos','marca de productos','marcas de productos'); // admin interface, singular, plural
+    public $downloadExcel=false; // Download Excel
+    public $downloadMsCsv=false; // Download MS CSV
+    public $downloadCsv=false; // Download CSV
+    public $nombre_anterior;
+    public $nombre_anterior2;
+    public $hideCreateAction = false;
+    public $hideListAction = false;
+    public $hideUpdateAction = true;
 	/**
 	 * Returns the static model of the specified AR class.
 	 * @param string $className active record class name.
@@ -31,6 +40,13 @@ class MarenasaProductoMarcas extends MActiveRecord
 		return 'marenasa_producto_marcas';
 	}
 
+	function behaviors() {
+    	return array(
+        'file' => array(
+            'class'=>'application.modules.ycm.behaviors.FileBehavior',
+           ),
+        );
+    }
 	/**
 	 * @return array validation rules for model attributes.
 	 */
@@ -65,11 +81,18 @@ class MarenasaProductoMarcas extends MActiveRecord
 	public function attributeLabels()
 	{
 		return array(
-			'id' => 'ID',
 			'marca' => 'Marca',
 			'logo' => 'Logo',
 		);
 	}
+	public function attributeWidgets()
+    {
+        return array
+        (
+            array('marca', 'textField'),
+            array('logo', 'image'),
+        );
+    }
 
 	/**
 	 * Retrieves a list of models based on the current search/filter conditions.
@@ -90,4 +113,205 @@ class MarenasaProductoMarcas extends MActiveRecord
 			'criteria'=>$criteria,
 		));
 	}
+    public function imagenWidget($nombre)
+    {
+        $b = Yii::app()->baseUrl;
+        return "<div class='imagenWidget' style='background-image:url($b/uploads/marenasaproductomarcas/logo/$nombre)'></div>";
+    }
+
+	public function extraOptions() 
+    {
+        $array = array
+        (
+            'logo'=>array
+            (
+                'class'=>'span5 jcropImage'
+            ),            
+        );
+        return $array;
+    }
+    public function js() 
+    {
+        $baseUrl = Yii::app()->baseUrl;
+        $cs = Yii::app()->getClientScript();        
+        $cs->registerScriptFile($baseUrl.'/js/plugins/JCrop/jquery.Jcrop.min.js');
+        $cs->registerScriptFile($baseUrl.'/js/plugins/ColorBox/jquery.colorbox.js');
+        $cs->registerScriptFile($baseUrl.'/js/ycm/productocategorias/cutImage.js');
+    }
+    public function css() 
+    {
+        $baseUrl = Yii::app()->baseUrl;
+        $cs = Yii::app()->getClientScript();
+        $cs->registerCssFile($baseUrl.'/css/plugins/JCrop/jquery.Jcrop.min.css');
+        $cs->registerCssFile($baseUrl.'/js/plugins/ColorBox/colorbox.css');
+        $cs->registerCssFile($baseUrl.'/css/ycm/productoscategoria/cutImage.css');
+        $cs->registerCssFile($baseUrl.'/css/ycm/productoscategoria/list.css');
+    }
+    public function adminSearch()
+    {
+        return array
+        (
+            'columns'=> array
+            (
+                array
+                (
+                    'name'=>'marca',
+                    'value'=>'$data->marca',
+                ),
+                array
+                (
+                    'name' => 'logo',
+                    'type' => 'raw',
+                    'value' => 'MarenasaProductoMarcas::model()->imagenWidget($data->logo)',
+                    'filter' => ''
+                ),
+            )
+        );
+    }
+    public function extraHtml() 
+    {
+        $array = array
+        (
+            'logo'=>'
+                    <!-- # FancyBox y jcrop div # -->
+                    <a id="clrbx" href="#jcrop"></a>
+                    <div style="display:none;">
+                        <div id="jcrop">
+                            <table>
+                                <tr>
+                                    <td id="image_container">
+                                    <img src="#" id="selected_image" alt="Imagen seleccionada"/>
+                                    </td>
+                                                                </tr>
+                                                                <tr>
+                                    <td>
+                                    <div id="miniPreview" style="">
+                                        <img src="#" id="preview" alt="vista previa" class="jcrop-preview" style="background-color:transparent;" />
+                                    </div>
+                                    <div id="miniButton">
+                                        <input type="button" id="getCrop" value="Recortar Imagen" />
+                                        <input type="hidden" id="x" name="x" value="" /> 
+                                        <input type="hidden" id="y" name="y" value="" /> 
+                                        <input type="hidden" id="w" name="w" value="" /> 
+                                        <input type="hidden" id="h" name="h" value="" /> 
+                                        <input type="hidden" id="divwidth" name="divwidth" value="" /> 
+                                        <input type="hidden" id="divheight" name="divheight" value="" /> 
+                                    </div>
+                                    </td>
+                                </tr>
+                                <tr>
+                                    <td style="text-align:right;" colspan="2">
+                                    </td>
+                                </tr>
+                            </table>
+                        </div>  
+                    </div>
+                    '
+            );
+        return $array;
+    }
+    public function extraPhpBeforeSaveValidate($model='',$post='',$paths='',$module='')
+    {
+    }
+    public function extraPhpAfterSaveValidate($model='',$post='',$paths='', $module='')
+    {   
+        if($paths != null && $paths != '')
+        {
+            for($i=0; $i < count($paths); $i++)
+            {
+                if(!isset($post['x'],$post['y'],$post['w'],$post['h']))
+                    throw new CHttpException(400,"No fue posible recortar la imagen, los parametros de recorte son invalidos");
+                //revisa que la imagen funte exista en el servidor
+                if(!file_exists($paths[$i]))
+                    throw new CHttpException(500,"La imagen no ha sido cargada correctamente");
+                //si no trae el ancho y el largo intenta mover $imagen a la ruta de $nuevaImagen
+                //tal cual sin recortar devido a que los parametros de recorte no son suficientes
+                if(empty($post['w']) || empty($post['h'])){
+                    return move_uploaded_file($paths[$i],$paths[$i]);
+                }
+                //width y height que tendra la imagen generada
+                $width=$post["divwidth"];
+                $height=$post["divheight"];
+                //crea una copia de la imagen en memoria
+                $_SESSION["tipo"] = '';
+                $_SESSION["creado"] = '';
+                $ext = pathinfo($paths[$i], PATHINFO_EXTENSION);
+                $type = strtolower($ext);
+                if ($type === 'jpeg') {$type = 'jpg';}
+                switch($type){
+                        case 'bmp': $img_r = imagecreatefromwbmp($paths[$i]); break;
+                        case 'gif': $img_r = imagecreatefromgif($paths[$i]); break;
+                        case 'jpg': $img_r = imagecreatefromjpeg($paths[$i]); break;
+                        case 'png': $img_r = imagecreatefrompng($paths[$i]); break;
+                        default : return "Unsupported picture type!, Please use one of the following: bmp, gif, jpg, jpeg or png";
+                }
+                $_SESSION["tipo"] = $type;
+                //crea una nueva imagen en memoria del tamaño definido por width y height
+                $dst_r = imagecreatetruecolor( (int)$width, (int)$height );
+                //calidad de la imagen final
+                $calidad=100;
+                // intentamos preservar la transparencia
+                if($type == "gif" || $type == "png"){
+                        $r = imagecolortransparent($dst_r, imagecolorallocatealpha($img_r, 0, 0, 0, 127));
+                        $_SESSION["creado"] = $r;
+                        imagealphablending($dst_r, false);
+                        imagesavealpha($dst_r, true);
+                }
+                //crea una imagen en memoria en base a la imagen fuente con los datos de recorte
+                $retorno= imagecopyresampled($dst_r,$img_r,0,0,(int)$post['x'],(int)$post['y'],
+                    (int)$width,(int)$height,(int)$post['w'],(int)$post['h']);
+                //si no logro crear la co[ia de la imagen en memoria
+                if($retorno === false){
+                    return false;
+                }
+                $n = strpos($paths[$i], "logo");  //variable de imagen
+                $new_path = substr($paths[$i], 0, $n);
+                //si la imagen es cambiada con un update, toma la imagen anterior y la intenta borrar
+
+                /* Nuevo PATH */
+//                $new_path = $module->getAttributePath(__CLASS__,'logo').DIRECTORY_SEPARATOR.'cutted_image'.DIRECTORY_SEPARATOR;
+                $new_path = $module->getAttributePath(__CLASS__,'logo').DIRECTORY_SEPARATOR;
+
+                if($this->nombre_anterior2 != null)
+                {
+                    $baseUrl = Yii::app()->baseUrl;
+                    $targetFile = $baseUrl."uploads/marenasaproductomarcas/logo/{$this->nombre_anterior2";
+                    unlink(realpath($targetFile));
+                }
+                $new_path = $new_path.$model->imagen;
+                //intenta guardar la imagen resultante en el servidor
+                                switch($type){
+                                        case 'bmp': $retorno = imagewbmp($dst_r, $new_path); break;
+                                        case 'gif': $retorno = imagegif($dst_r, $new_path) ; break;
+                                        case 'jpg': $retorno = imagejpeg($dst_r, $new_path,$calidad); break;
+                                        case 'png': $retorno = imagepng($dst_r, $new_path) ; break;
+                                }
+                if($retorno === false){
+                    return false;
+                }
+            }
+        }
+        else
+        {
+             $columnas = array
+             (
+                 'logo' => $model->nombre_anterior
+             );
+             $update = Yii::app()->db->createCommand()
+                 ->update('oko_muebles', $columnas, "id = ".(int)$model->id);
+        }
+    }
+    public function afterFind()
+    {
+        if($this->logo != '' && $this->logo != null)
+        {
+            $this->nombre_anterior = $this->logo;
+            $this->nombre_anterior2 = $this->logo;
+        }
+        else
+        {
+            $this->nombre_anterior = null;
+            $this->$nombre_anterior2 = null;
+        }
+    }
 }
