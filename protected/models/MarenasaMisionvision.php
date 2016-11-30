9c239a209a7216e1e8ad72afd04ff573a75d2c01
MidoriHeart <midori.wings@gmail.com>
2016-11-30 15:47:51 -0800

    cambios

--- protected/models/MarenasaMisionvision.php
+++ protected/models/MarenasaMisionvision.php
@@ -43,8 +43,7 @@ class MarenasaMisionvision extends MActiveRecord
 		// NOTE: you should only define rules for those attributes that
 		// will receive user inputs.
 		return array(
-			array('id, mision, vision', 'required'),
-			array('id', 'numerical', 'integerOnly'=>true),
+			array('mision, vision', 'required'),
 			array('mision, vision', 'length', 'max'=>300),
 			// The following rule is used by search().
 			// @todo Please remove those attributes that should not be searched.
