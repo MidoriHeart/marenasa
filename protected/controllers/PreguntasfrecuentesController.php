bec39ce1134a2c16eb8970a872f5ce75e4c9e966
SirGamboa <gamboa.leonardo.1990@gmail.com>
2016-12-09 08:59:48 -0800

    ya se puede abrir preguntas frecuentes

--- protected/controllers/PreguntasfrecuentesController.php
+++ protected/controllers/PreguntasfrecuentesController.php
@@ -2,15 +2,16 @@
 
 class PreguntasfrecuentesController extends Controller
 {
-	public function actionIndex()
-	{
-		$categorias = MarenasaPreguntasCategoria::model()->findAll();
-		$preguntas = MarenasaPreguntas::model()->findAll();
-		$this->render('index', array('categorias'=>$categorias,'preguntas_freq'=>$preguntas));
-
-	}
-
-
+    public function actionIndex()
+    {
+        $categorias = MarenasaPreguntasCategoria::model()->findAll();
+        $preguntas = MarenasaPreguntas::model()->findAll();
+        $this->render('index', array
+            (
+                'categorias'=>$categorias,
+                'preguntas_freq'=>$preguntas
+            ));
+    }
 	// Uncomment the following methods and override them if needed
 	/*
 	public function filters()
