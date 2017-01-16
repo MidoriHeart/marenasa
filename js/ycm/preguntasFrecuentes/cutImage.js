431f7088a40de02a93d6c6af690c10762abd004d
Omar Peralta <ob.peralta03@gmail.com>
2017-01-16 14:01:23 -0800

    subiendo cambios de tamaño en preguntas frecuentes

--- js/ycm/preguntasFrecuentes/cutImage.js
+++ js/ycm/preguntasFrecuentes/cutImage.js
@@ -15,7 +15,7 @@ $(document).ready(function()
         },
         onComplete:function()
         {
-            jcrop_api.setSelect([0,0,500,286]);
+            jcrop_api.setSelect([0,0,350,194]);
             var w_width = $(window).width()-($(window).width()*0.15)-255;
             if($('#selected_image').width()<w_width )
                 w_width=$('#selected_image').width();
@@ -69,8 +69,8 @@ $(document).ready(function()
     {
         if (parseInt(c.w) > 0)
         {
-            var rx = 500 / c.w;
-            var ry = 286 / c.h;
+            var rx = 700 / c.w;
+            var ry = 388 / c.h;
             $('#preview').css(
             {
                 width: Math.round(rx * boundx) + 'px',
@@ -108,7 +108,7 @@ $(document).ready(function()
                     boxHeight: $(window).height()-($(window).height()*0.20),
                     onChange: updatePreview,
                     onSelect: updatePreview,
-                    aspectRatio: 7/4
+                    aspectRatio: 175/97
                 },function()
                 {
                     var bounds = this.getBounds();
